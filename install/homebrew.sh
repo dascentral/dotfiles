17d9a87c9528571553ef9a3424d90b59c75a8471
Douglas Smith <smithda@gmail.com>
2017-02-26 08:13:28 -0500

    Updating Homebrew if installed

--- install/homebrew.sh
+++ install/homebrew.sh
@@ -9,14 +9,16 @@ name="Homebrew"
 check="/usr/local/bin/brew"
 
 # Check if installed
-printf "\033[1mInstalling $name...\033[0m"
 if [ -e $check ]; then
-    printf "\033[1m already installed.\n\n\033[0m"
+    printf "\033[1mBrew update...\033[0m\n"
+    brew update
+    printf "\n"
     exit 0;
 fi
 printf "\n"
 
 # Install
+printf "\033[1mInstalling $name...\033[0m"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Update before use
