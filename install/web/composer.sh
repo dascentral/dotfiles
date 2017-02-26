acd6a9fa4c579df92892a4c7731705d125ac7581
Douglas Smith <smithda@gmail.com>
2017-02-26 07:55:17 -0500

    Updating composer if installed

--- install/web/composer.sh
+++ install/web/composer.sh
@@ -9,14 +9,15 @@ name="Composer"
 check="/usr/local/bin/composer"
 
 # Check if installed
-printf "\033[1mInstalling $name...\033[0m"
 if [ -e $check ]; then
-    printf "\033[1m already installed.\n\n\033[0m"
+    printf "\033[1mUpdating $name...\033[0m\n"
+    /usr/local/bin/composer self-update
     exit 0;
 fi
 printf "\n"
 
 # Install
+printf "\033[1mInstalling $name...\033[0m"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
