36e385c38d673f44c5bd9b9bc1f73efacc9a6a6d
Douglas Smith <smithda@gmail.com>
2017-03-04 09:06:10 -0500

    Adding preferences to Sublime installer

--- install/sublime.sh
+++ install/sublime.sh
@@ -4,11 +4,15 @@
 # Sublime Text
 #===================================================================================
 
-# Put snippets in place
-printf "\033[1mCopying Sublime Text snippets...\033[0m"
+printf "\033[1mInstalling Sublime Text items...\033[0m"
+
+# Preferences
+cp sublime/preferences/Preferences.sublime-settings /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+
+# Snippets
 rm /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets/*.sublime-snippet
 cp sublime/snippets/* /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets
-printf "\033[1m complete.\033[0m"
 
 # Wrap it up
+printf "\033[1m complete.\033[0m"
 printf "\n\n"
