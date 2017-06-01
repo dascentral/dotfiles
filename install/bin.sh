b44f0a650b2921804c2e158f2d35a3e76d656d03
Douglas Smith <smithda@gmail.com>
2017-06-01 17:32:36 -0400

    Using full path for source of bin scripts

--- install/bin.sh
+++ install/bin.sh
@@ -6,7 +6,7 @@
 
 # Put /usr/local/bin scripts in place
 printf "\033[1mCopying scripts to /usr/local/bin...\033[0m"
-cp bin/* /usr/local/bin
+cp /Users/$USER/Workspace/dotfiles/bin/* /usr/local/bin
 printf "\033[1m complete.\033[0m"
 
 # Wrap it up
