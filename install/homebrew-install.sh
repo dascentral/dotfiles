7e1790a190584a5809284470030dd44803dba0de
Douglas Smith <smithda@gmail.com>
2017-07-24 08:23:16 -0400

    Updating Homebrew install messaging

--- install/homebrew-install.sh
+++ install/homebrew-install.sh
@@ -9,8 +9,10 @@ name="Homebrew"
 check="/usr/local/bin/brew"
 
 # Install if not installed
-if [ ! -e $check ]; then
-    printf "\033[1mInstalling $name...\033[0m"
+printf "\033[1mInstalling $name...\033[0m"
+if [ -e $check ]; then
+    printf "\033[1m already installed.\n\n\033[0m"
+else
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew update
     printf "\n\n"
