998c1011ccb5708261cf4ccd434cc2e39c329139
Douglas Smith <smithda@gmail.com>
2017-08-10 18:59:52 -0400

    Adding PHP Companion settings to Sublime install

--- install/sublime.sh
+++ install/sublime.sh
@@ -9,6 +9,9 @@ printf "\033[1mInstalling Sublime Text items...\033[0m"
 # Preferences
 cp sublime/preferences/Preferences.sublime-settings /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
+# PHP Companion
+cp sublime/preferences/PHP\ Companion.sublime-settings /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+
 # Snippets
 rm /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets/*.sublime-snippet
 cp sublime/snippets/* /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets
