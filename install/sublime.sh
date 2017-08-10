9a1401f6c22cd2b60d7304474ee24a5a1004bc19
Douglas Smith <smithda@gmail.com>
2017-08-10 19:08:38 -0400

    Escaping ()

--- install/sublime.sh
+++ install/sublime.sh
@@ -13,7 +13,7 @@ cp sublime/preferences/Preferences.sublime-settings /Users/$USER/Library/Applica
 cp sublime/preferences/PHP\ Companion.sublime-settings /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
 # Key Bindings
-cp sublime/preferences/Default\ (OSX).sublime-keymap /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+cp sublime/preferences/Default\ \(OSX\).sublime-keymap /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
 # Snippets
 rm /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets/*.sublime-snippet
