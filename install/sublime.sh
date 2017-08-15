38714b9b3cc967ed78a6911e8c41c6b9cbbb48a6
Douglas Smith <smithda@gmail.com>
2017-08-15 11:45:51 -0400

    Updating Sublime install.
    Adding additional output. Adding PHP CS Fixer settings

--- install/sublime.sh
+++ install/sublime.sh
@@ -4,25 +4,29 @@
 # Sublime Text
 #===================================================================================
 
-printf "\033[1mInstalling Sublime Text items...\033[0m"
+printf "\033[1mInstalling Sublime Text items...\033[0m\n"
 
 # Preferences
+printf "\033[33mPreferences\033[0m\n"
 cp /Users/$USER/Workspace/dotfiles/sublime/preferences/Preferences.sublime-settings /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
 # Key Bindings
+printf "\033[33mKey Bindings\033[0m\n"
 cp /Users/$USER/Workspace/dotfiles/sublime/preferences/Default\ \(OSX\).sublime-keymap /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
 # Snippets
+printf "\033[33mSnippets\033[0m\n"
 rm /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets/*.sublime-snippet
 cp /Users/$USER/Workspace/dotfiles/sublime/snippets/* /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets
 
 # PHP Companion
+printf "\033[33mPHP Companion Settings\033[0m\n"
 cp /Users/$USER/Workspace/dotfiles/sublime/preferences/PHP\ Companion.sublime-settings /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
 # PHP CS Fixer
+printf "\033[33mPHP CS Fixer Settings\033[0m\n"
 cp /Users/$USER/Workspace/dotfiles/sublime/preferences/SublimePhpCsFixer.sublime-settings /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-sudo sed -i "s/USERNAME/$USER/g" /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/SublimePhpCsFixer.sublime-settings
+sed -i '.original' "s/USERNAME/$USER/g" /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/SublimePhpCsFixer.sublime-settings
 
 # Wrap it up
-printf "\033[1m complete.\033[0m"
 printf "\n\n"
