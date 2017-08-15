1e429a6816c548579c5e2bab878d96a4901b37d6
Douglas Smith <smithda@gmail.com>
2017-08-15 11:36:30 -0400

    Adding full paths

--- install/sublime.sh
+++ install/sublime.sh
@@ -7,17 +7,17 @@
 printf "\033[1mInstalling Sublime Text items...\033[0m"
 
 # Preferences
-cp sublime/preferences/Preferences.sublime-settings /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-
-# PHP Companion
-cp sublime/preferences/PHP\ Companion.sublime-settings /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+cp /Users/$USER/Workspace/dotfiles/sublime/preferences/Preferences.sublime-settings /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
 # Key Bindings
-cp sublime/preferences/Default\ \(OSX\).sublime-keymap /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+cp /Users/$USER/Workspace/dotfiles/sublime/preferences/Default\ \(OSX\).sublime-keymap /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
 # Snippets
 rm /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets/*.sublime-snippet
-cp sublime/snippets/* /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets
+cp /Users/$USER/Workspace/dotfiles/sublime/snippets/* /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets
+
+# PHP Companion
+cp /Users/$USER/Workspace/dotfiles/sublime/preferences/PHP\ Companion.sublime-settings /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
 # Wrap it up
 printf "\033[1m complete.\033[0m"
