94e68f08b11cc4c66ea856be2b837748d31f25ce
Douglas Smith <smithda@gmail.com>
2017-08-24 10:34:11 -0400

    Removing Sublime items. Moved to Dropbox.

--- install/sublime.sh
+++ install/sublime.sh
@@ -4,29 +4,11 @@
 # Sublime Text
 #===================================================================================
 
-printf "\033[1mInstalling Sublime Text items...\033[0m\n"
+printf "\033[1mAdding Sublime Text symbolic link...\033[0m\n"
 
-# Preferences
-printf "\033[33mPreferences\033[0m\n"
-cp /Users/$USER/Workspace/dotfiles/sublime/preferences/Preferences.sublime-settings /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-
-# Key Bindings
-printf "\033[33mKey Bindings\033[0m\n"
-cp /Users/$USER/Workspace/dotfiles/sublime/preferences/Default\ \(OSX\).sublime-keymap /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-
-# Snippets
-printf "\033[33mSnippets\033[0m\n"
-rm /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets/*.sublime-snippet
-cp /Users/$USER/Workspace/dotfiles/sublime/snippets/* /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets
-
-# PHP Companion
-printf "\033[33mPHP Companion Settings\033[0m\n"
-cp /Users/$USER/Workspace/dotfiles/sublime/preferences/PHP\ Companion.sublime-settings /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-
-# PHP CS Fixer
-printf "\033[33mPHP CS Fixer Settings\033[0m\n"
-cp /Users/$USER/Workspace/dotfiles/sublime/preferences/SublimePhpCsFixer.sublime-settings /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-sed -i '.original' "s/USERNAME/$USER/g" /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/SublimePhpCsFixer.sublime-settings
+if [ ! -e "/usr/local/bin/subl" ]; then
+   ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+fi
 
 # Wrap it up
 printf "\n"
