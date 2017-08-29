7a63562ae49420e082d3233a08398a28d5aee3a0
Douglas Smith <smithda@gmail.com>
2017-08-29 12:30:16 -0400

    Updating the Sublime Text installation.
    Creating a local "~/.sublime" folder. Applying SublimePhpCsFixer settings.

--- install/sublime.sh
+++ install/sublime.sh
@@ -4,10 +4,24 @@
 # Sublime Text
 #===================================================================================
 
-printf "\033[1mAdding Sublime Text symbolic link...\033[0m\n"
+# Local Sublime Settings - Handled silenty
+if [ ! -e "/Users/${USER}/.sublime" ]; then
+    mkdir /Users/${USER}/.sublime
+fi
 
+# Symbolic link
 if [ ! -e "/usr/local/bin/subl" ]; then
-   ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+    printf "\033[1mAdding Sublime Text symbolic link...\033[0m\n"
+    ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+    printf "\033[1m complete.\033[0m\n\n"
+fi
+
+# SublimePhpCsFixer Settings
+if [ ! -e "/Users/${USER}/.sublime/SublimePhpCsFixer.sublime-settings" ]; then
+    printf "\033[1mConfiguring SublimePhpCsFixer...\033[0m\n"
+    cp /Users/${USER}/Workspace/dotfiles/sublime/SublimePhpCsFixer.sublime-settings /Users/${USER}/.sublime
+    sudo sed -i "s/USERNAME/${USER}/g" /Users/${USER}/.sublime/SublimePhpCsFixer.sublime-settings
+    printf "\033[1m complete.\033[0m\n\n"
 fi
 
 # Wrap it up
