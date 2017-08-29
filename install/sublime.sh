46d83d667020fd4fd677ea216f25b2733c07b05f
Douglas Smith <smithda@gmail.com>
2017-08-29 12:34:17 -0400

    Whitespacing

--- install/sublime.sh
+++ install/sublime.sh
@@ -11,14 +11,14 @@ fi
 
 # Symbolic link
 if [ ! -e "/usr/local/bin/subl" ]; then
-    printf "\033[1mAdding Sublime Text symbolic link...\033[0m\n"
+    printf "\033[1mAdding Sublime Text symbolic link...\033[0m"
     ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
     printf "\033[1m complete.\033[0m\n\n"
 fi
 
 # SublimePhpCsFixer Settings
 if [ ! -e "/Users/${USER}/.sublime/SublimePhpCsFixer.sublime-settings" ]; then
-    printf "\033[1mConfiguring SublimePhpCsFixer...\033[0m\n"
+    printf "\033[1mConfiguring SublimePhpCsFixer...\033[0m"
     cp /Users/${USER}/Workspace/dotfiles/sublime/SublimePhpCsFixer.sublime-settings /Users/${USER}/.sublime
     sed -i '.original' "s/USERNAME/${USER}/g" /Users/${USER}/.sublime/SublimePhpCsFixer.sublime-settings
     printf "\033[1m complete.\033[0m\n\n"
