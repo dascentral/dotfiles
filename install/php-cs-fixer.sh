5eccd027f8aa1c7d341dc375291536e101ad94f5
Douglas Smith <smithda@gmail.com>
2017-09-07 12:08:57 -0400

    Updating copy

--- install/php-cs-fixer.sh
+++ install/php-cs-fixer.sh
@@ -17,6 +17,6 @@ else
 fi
 
 # Add PHP CS Fixer configuration
-printf "\033[1;33mConfiguring $name... "
+printf "\033[1;33mConfiguring $name.\033[0m"
 cp /Users/$USER/Workspace/dotfiles/settings/php/.phpcsfixer /Users/$USER
-printf "complete.\033[0m\n\n"
+printf "\n\n"
