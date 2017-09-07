08b9410e0bb9dcf987b992bb7e6bf9781b6d7110
Douglas Smith <smithda@gmail.com>
2017-09-07 11:55:12 -0400

    Color coding

--- install/wordpress.sh
+++ install/wordpress.sh
@@ -9,12 +9,10 @@ name="WP-CLI"
 check="/usr/local/bin/wp"
 
 # Check if installed
-printf "\033[1mInstalling $name...\033[0m"
 if [ -e $check ]; then
-    printf "\033[1m already installed.\n\n\033[0m"
+    printf "\033[1;37m$name already installed.\n\n\033[0m"
     exit 0;
 fi
-printf "\n"
 
 # Install
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
