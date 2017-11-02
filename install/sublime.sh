3179d5e4659a2a70d57a3543e4a52dedb23d0b6b
Douglas Smith <smithda@gmail.com>
2017-11-02 11:18:13 -0400

    Creation of the symbolic link moved to setup

--- install/sublime.sh
+++ install/sublime.sh
@@ -9,13 +9,6 @@ if [ ! -e "/Users/${USER}/.sublime" ]; then
     mkdir /Users/${USER}/.sublime
 fi
 
-# Symbolic link
-if [ ! -e "/usr/local/bin/subl" ]; then
-    printf "\033[1;37mAdding Sublime Text symbolic link..."
-    ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
-    printf " complete.\033[0m\n\n"
-fi
-
 # SublimePhpCsFixer Settings
 if [ ! -e "/Users/${USER}/.sublime/SublimePhpCsFixer.sublime-settings" ]; then
     printf "\033[1;37mConfiguring SublimePhpCsFixer..."
