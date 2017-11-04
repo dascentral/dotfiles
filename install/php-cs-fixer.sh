69627012c5a22abdcc339acd8a50d2e2c659c087
Douglas Smith <smithda@gmail.com>
2017-11-04 08:22:17 -0400

    Removing the settings component of the install script

--- install/php-cs-fixer.sh
+++ install/php-cs-fixer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #===================================================================================
-# PHP CS Fixer
+# PHP-CS-Fixer
 #===================================================================================
 
 # setup
@@ -15,12 +15,3 @@ else
     printf "\033[1mInstalling $name...\033[0m\n"
     composer global require friendsofphp/php-cs-fixer
 fi
-
-# Add PHP CS Fixer configuration
-config="/usr/local/var/php-cs-fixer"
-printf "\033[1;33mConfiguring $name.\033[0m"
-if [ ! -e $config ]; then
-    mkdir $config
-fi
-cp /Users/$USER/Workspace/dotfiles/settings/php/.phpcsfixer $config/
-printf "\n\n"
