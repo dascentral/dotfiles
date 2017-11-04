b10a807ca5b599ccbf7c239257113387b40d403f
Douglas Smith <smithda@gmail.com>
2017-11-04 08:29:24 -0400

    Switching form elipses to periods

--- update/composer-update.sh
+++ update/composer-update.sh
@@ -9,7 +9,7 @@ check="/usr/local/bin/composer"
 
 # Update if installed
 if [ -e $check ]; then
-    printf "\033[1;37mComposer self-update...\033[0m\n"
+    printf "\033[1;37mComposer self-update.\033[0m\n"
     /usr/local/bin/composer self-update
     printf "\n"
 fi
--- update/homebrew-cleanup.sh
+++ update/homebrew-cleanup.sh
@@ -9,7 +9,7 @@ check="/usr/local/bin/brew"
 
 # Update if installed
 if [ -e $check ]; then
-    printf "\033[1;37mBrew cleanup...\033[0m\n"
+    printf "\033[1;37mBrew cleanup.\033[0m\n"
     brew cleanup
     printf "\n"
 fi
