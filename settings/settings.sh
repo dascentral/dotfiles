697830095008d87c656e6871a02fdf8ea049b484
Douglas Smith <smithda@gmail.com>
2017-11-06 08:01:41 -0500

    Adding a slow of macOS settings

--- settings/settings.sh
+++ settings/settings.sh
@@ -1,6 +1,73 @@
 #!/bin/sh
 
+#===================================================================================
+# Resources:
+# - https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+#===================================================================================
+
 printf "\033[1;37mSynchronizing Mac Settings.\033[0m\n\n"
+
+
+
+#===================================================================================
+# Setup
+#===================================================================================
+
+# Close any open System Preferences panes. Prevents overriding changes below.
+osascript -e 'tell application "System Preferences" to quit'
+
+
+
+#===================================================================================
+# macOS - General
+#===================================================================================
+
+# Always show scrollbars
+defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+# Possible values: `WhenScrolling`, `Automatic` and `Always`
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Expand print panel by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Disable automatic capitalization as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Disable automatic period substitution as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Set the default wallpaper image - Primarily used on login screen
+#rm -rf ~/Library/Application Support/Dock/desktoppicture.db
+#sudo rm -rf /System/Library/CoreServices/DefaultDesktop.jpg
+#sudo ln -s /path/to/your/image /System/Library/CoreServices/DefaultDesktop.jpg
+
+
+
+#===================================================================================
+# macOS - Trackpad, mouse, keyboard, Bluetooth accessories, and input
+#===================================================================================
+
+# Enable full keyboard access for all controls
+# (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+
 
 #===================================================================================
 # macOS - Finder
@@ -25,6 +92,7 @@ defaults write com.apple.finder ShowPathbar -bool true
 chflags nohidden ~/Library
 
 
+
 #===================================================================================
 # iTerm2
 #===================================================================================
@@ -42,6 +110,7 @@ cp /Users/${USER}/Workspace/dotfiles/settings/iTerm2/custom/plugins/git/git.plug
 cp /Users/${USER}/Workspace/dotfiles/settings/iTerm2/custom/themes/af-magic.zsh-theme /Users/${USER}/.oh-my-zsh/custom/themes
 
 
+
 #===================================================================================
 # Sublime Text - SublimePhpCsFixer
 #===================================================================================
@@ -54,6 +123,7 @@ fi
 # Copy settings into place
 cp /Users/${USER}/Workspace/dotfiles/sublime/SublimePhpCsFixer.sublime-settings /Users/${USER}/.sublime
 sed -i '.original' "s/USERNAME/${USER}/g" /Users/${USER}/.sublime/SublimePhpCsFixer.sublime-settings
+
 
 
 #===================================================================================
