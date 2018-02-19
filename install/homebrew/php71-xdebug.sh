087278df17dbb93796858479d1cc55a38763f646
Douglas Smith <smithda@gmail.com>
2018-02-19 07:33:42 -0500

    Ensuring extension is loaded during install

--- install/homebrew/php71-xdebug.sh
+++ install/homebrew/php71-xdebug.sh
@@ -12,5 +12,8 @@ check="/usr/local/Cellar/php71-xdebug"
 if [ ! -e $check ]; then
     printf "\033[1;37mInstalling $name.\033[0m\n"
     brew install php71-xdebug
+
+    # Ensure the extension is loaded
+    printf "xdebug.remote_enable=1" >> /usr/local/etc/php/7.1/conf.d/ext-xdebug.ini
     printf "\n\n"
 fi
