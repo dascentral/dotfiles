c6e50fa0e0795f7f2d395e204e1434eb7668c26e
Douglas Smith <smithda@gmail.com>
2018-02-19 07:24:06 -0500

    Adjusting color during install

--- install/homebrew/php71-xdebug.sh
+++ install/homebrew/php71-xdebug.sh
@@ -10,7 +10,7 @@ check="/usr/local/Cellar/php71-xdebug"
 
 # Installation
 if [ ! -e $check ]; then
-    printf "\033[1;33mInstalling $name.\033[0m\n"
+    printf "\033[1;37mInstalling $name.\033[0m\n"
     brew install php71-xdebug
     printf "\n\n"
 fi
