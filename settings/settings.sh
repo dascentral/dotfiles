18e38a49e488568f805e0cda77e426bd9c1a05e8
Douglas Smith <smithda@gmail.com>
2018-02-26 08:11:46 -0500

    Adding some Chrome settings

--- settings/settings.sh
+++ settings/settings.sh
@@ -109,6 +109,19 @@ defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
 
 #===================================================================================
+# Google Chrome & Google Chrome Canary
+#===================================================================================
+
+# Disable the all too sensitive backswipe on trackpads
+defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
+defaults write com.google.Chrome.canary AppleEnableSwipeNavigateWithScrolls -bool false
+
+# Disable the all too sensitive backswipe on Magic Mouse
+defaults write com.google.Chrome AppleEnableMouseSwipeNavigateWithScrolls -bool false
+defaults write com.google.Chrome.canary AppleEnableMouseSwipeNavigateWithScrolls -bool false
+
+
+#===================================================================================
 # iTerm2
 #===================================================================================
 
