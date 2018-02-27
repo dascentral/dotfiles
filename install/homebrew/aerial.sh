eac2bf45047c66db14552acc0e6210031dc53c13
Douglas Smith <smithda@gmail.com>
2018-02-26 22:24:32 -0500

    Fixing reference to check with space

--- install/homebrew/aerial.sh
+++ install/homebrew/aerial.sh
@@ -9,7 +9,7 @@ name="Aerial Screen Saver"
 check="/Users/${USER}/Library/Screen Savers/Aerial.saver"
 
 # Installation
-if [ ! -e $check ]; then
+if [ ! -e "$check" ]; then
     printf "\033[1;37mInstalling $name.\033[0m\n"
     brew cask install aerial
     printf "\n\n"
