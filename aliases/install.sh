ed7b7baea93b4b1b030ef22986093712aa6cb493
Douglas Smith <smithda@gmail.com>
2018-04-01 21:11:07 -0400

    Adding an installation script for aliases

---
+++ aliases/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#===================================================================================
+# Alias Installlation
+#===================================================================================
+
+# Start it up
+printf "\033[1;37mInstalling aliases.\033[0m"
+
+# Copy into place
+cp /Users/$USER/dotfiles/aliases /Users/$USER/.aliases
+
+# Include any custom aliases stored locally
+if [ -e "/Users/$USER/.aliases-local" ]; then
+    cat /Users/$USER/.aliases-local >> /Users/$USER/.aliases
+fi
+
+# Wrap it up
+printf "\n\n"
