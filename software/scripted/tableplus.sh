61a23694f8050cfc97ccb937bfe7c09daa0d6d17
Douglas Smith <douglas.smith@akqa.com>
2019-06-14 11:16:48 -0400

    Updating syntax

--- software/scripted/tableplus.sh
+++ software/scripted/tableplus.sh
@@ -5,12 +5,13 @@
 #===================================================================================
 
 # setup
+app="tableplus"
 name="TablePlus"
 check="/Applications/TablePlus.app"
 
 # Installation
 if [ ! -e $check ]; then
     printf "\033[1;37mInstalling $name.\033[0m\n"
-    brew cask install tableplus
+    brew cask install $app
     printf "\n\n"
 fi
