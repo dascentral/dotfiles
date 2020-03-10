9155bd3127864709d4941548c4bb12b48524cb97
Douglas Smith <douglas.smith@akqa.com>
2020-03-10 10:47:10 -0400

    Adding Sublime Text

--- software/install.sh
+++ software/install.sh
@@ -343,3 +343,15 @@ if [ ! -e "$check" ]; then
 fi
 
 
+# Sublime Text
+app="sublime-text"
+name="Sublime Text"
+check="/Applications/Sublime Text.app"
+url="https://www.sublimetext.com/"
+if [ ! -e "$check" ]; then
+    printf "\033[1;37mInstalling $name - $url\033[0m\n"
+    brew cask install $app
+    printf "\n\n"
+fi
+
+
