fce09d84382f49f880e381d7cc60b5f2862e1eb0
Douglas Smith <douglas.smith@akqa.com>
2020-03-10 10:47:30 -0400

    Adding WhatsApp

--- software/install.sh
+++ software/install.sh
@@ -379,3 +379,15 @@ if [ ! -e "$check" ]; then
 fi
 
 
+# WhatsApp
+app="whatsapp"
+name="WhatsApp"
+check="/Applications/WhatsApp.app"
+url="https://www.whatsapp.com"
+if [ ! -e "$check" ]; then
+    printf "\033[1;37mInstalling $name - $url\033[0m\n"
+    brew cask install $app
+    printf "\n\n"
+fi
+
+
