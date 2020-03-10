a3c64030be6c0182f7e08d4c9319ff32fcf78d48
Douglas Smith <douglas.smith@akqa.com>
2020-03-10 10:44:48 -0400

    Adding Aerial

--- software/install.sh
+++ software/install.sh
@@ -130,3 +130,15 @@ if [ ! -e "$check" ]; then
 fi
 
 
+# Aerial
+app="aerial"
+name="Aerial Screen Saver"
+check="/Users/${USER}/Library/Screen Savers/Aerial.saver"
+url="https://github.com/JohnCoates/Aerial"
+if [ ! -e "$check" ]; then
+    printf "\033[1;37mInstalling $name - $url.\033[0m\n"
+    brew cask install $app
+    printf "\n\n"
+fi
+
+
