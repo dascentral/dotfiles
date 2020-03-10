014c58585d68046dbde1b3b098cb4bc8acf3fb9e
Douglas Smith <douglas.smith@akqa.com>
2020-03-10 10:22:51 -0400

    Moving composer install commands into install.sh

--- software/install.sh
+++ software/install.sh
@@ -1,29 +1,89 @@
 #!/bin/sh
 
 #===================================================================================
-# Confirm Homebrew & Composer are installed
+# Confirm necessary software is installed
 #===================================================================================
 
-# Check if installed
+# Homebrew
 if [ ! -e "/usr/local/bin/brew" ]; then
     printf "\n\033[1;33mHomebrew has not been installed. Aborting installation.\n\n\033[0m"
     exit 0;
 fi
 
-# Check if installed
+# Composer
 if [ ! -e "/usr/local/bin/composer" ]; then
     printf "\n\033[1;33mComposer has not been installed. Aborting installation.\n\n\033[0m"
     exit 0;
 fi
 
+# PHP
+if [ ! -e "/usr/local/bin/php" ]; then
+    printf "\n\033[1;33mPHP has not been installed. Aborting installation.\n\n\033[0m"
+    exit 0;
+fi
+
+
 
 #===================================================================================
-# Composer
+# Let's do this
 #===================================================================================
-/Users/${USER}/dotfiles/software/scripted/composer.sh
+
+# Composer Update
+printf "\033[1;37mComposer self-update.\033[0m\n"
+/usr/local/bin/composer self-update
+printf "\n"
 
 
-#===================================================================================
-# Homebrew
-#===================================================================================
-/Users/${USER}/dotfiles/software/scripted/homebrew.sh
+# Composer Global Require
+name="Composer Global Require"
+check="/Users/$USER/.composer/vendor/bin/cgr"
+url="https://github.com/consolidation/cgr"
+if [ ! -e "$check" ]; then
+    printf "\033[1;37mInstalling $name - $url\033[0m\n"
+    composer global require consolidation/cgr
+    printf "\n"
+fi
+
+
+# dascentral/hubflow-release
+name="dascentral/hubflow-release"
+check="/Users/$USER/.composer/vendor/bin/hf"
+url="https://packagist.org/packages/dascentral/hubflow-release"
+if [ ! -e "$check" ]; then
+    printf "\033[1;37mInstalling $name - $url\033[0m\n"
+    composer global require dascentral/hubflow-release
+    printf "\n"
+fi
+
+
+# laravel/installer
+name="laravel/installer"
+check="/Users/$USER/.composer/vendor/bin/laravel"
+url="https://laravel.com/docs/master"
+if [ ! -e "$check" ]; then
+    printf "\033[1;37mInstalling $name - $url\033[0m\n"
+    composer global require laravel/installer
+    printf "\n"
+fi
+
+
+# PHP Code Sniffer
+name="PHP Code Sniffer"
+check="/Users/$USER/.composer/vendor/squizlabs/php_codesniffer"
+url="https://github.com/squizlabs/PHP_CodeSniffer"
+if [ ! -e "$check" ]; then
+    printf "\033[1;37mInstalling $name.\033[0m\n"
+    composer global require "squizlabs/php_codesniffer=*"
+    printf "\n"
+fi
+
+
+# PHP CS Fixer
+name="PHP CS Fixer"
+check="/Users/$USER/.composer/vendor/friendsofphp/php-cs-fixer"
+url="https://github.com/FriendsOfPHP/PHP-CS-Fixer"
+if [ ! -e "$check" ]; then
+    printf "\033[1;37mInstalling $name.\033[0m\n"
+    composer global require friendsofphp/php-cs-fixer
+    printf "\n"
+fi
