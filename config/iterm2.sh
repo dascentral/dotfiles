0c0850793923b566395d8affcafa3a5ef92e9f79
Douglas Smith <douglas.smith@akqa.com>
2020-03-10 12:46:30 -0400

    Adding iTerm2 config

---
+++ config/iterm2.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/Documents/Software/iTerm2"
+
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+# Don’t display the annoying prompt when quitting iTerm
+defaults write com.googlecode.iterm2 PromptOnQuit -bool false
