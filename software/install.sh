6ee62acbb3d52f501e41ca8b2ddf2162ad5fd437
Douglas Smith <douglas.smith@akqa.com>
2020-03-10 10:44:57 -0400

    Adding Bartender 3

--- software/install.sh
+++ software/install.sh
@@ -142,3 +142,15 @@ if [ ! -e "$check" ]; then
 fi
 
 
+# Bartender
+app="bartender"
+name="Bartender"
+check="/Applications/Bartender 3.app"
+url="https://www.macbartender.com"
+if [ ! -e "$check" ]; then
+    printf "\033[1;37mInstalling $name - $url\033[0m\n"
+    brew cask install $app
+    printf "\n\n"
+fi
+
+
