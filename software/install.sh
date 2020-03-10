5a91e6e934a3b27f2ddcb1c0c4ed7cfa5eadac82
Douglas Smith <douglas.smith@akqa.com>
2020-03-10 10:46:32 -0400

    Adding Quick Look Plugins

--- software/install.sh
+++ software/install.sh
@@ -272,3 +272,14 @@ if [ ! -e "$check" ]; then
 fi
 
 
+# Quick Look Plugins
+name="Quick Look Plugins"
+check="/Users/${USER}/Library/QuickLook/QuickLookJSON.qlgenerator"
+url="https://github.com/sindresorhus/quick-look-plugins"
+if [ ! -e "$check" ]; then
+    printf "\033[1;37mInstalling $name - $url\033[0m\n"
+    brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json webpquicklook suspicious-package quicklookase qlvideo
+    printf "\n\n"
+fi
+
+
