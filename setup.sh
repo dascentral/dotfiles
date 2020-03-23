b6dbf6ecc51bd6baf8ef0c03338bee2ebf43d738
Douglas Smith <douglas.smith@akqa.com>
2020-03-23 16:52:11 -0400

    Moving Homebrew to the top.
    Since PHP needs it to install itself.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
 #============================================================================
+# Homebrew
+#============================================================================
+if [ -e /usr/local/bin/brew ]; then
+    printf "\n\033[1;37mHomebrew has already been installed.\033[0m\n\n"
+else
+    printf "\n\033[1;37mInstalling Homebrew.\033[0m\n"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    printf "\n\n"
+fi
+
+
+#============================================================================
 # PHP
 #============================================================================
 if [ -e /usr/local/bin/php ]; then
@@ -22,17 +34,5 @@ else
     printf "\n\033[1;37mInstalling Composer.\033[0m\n"
     php -r "readfile('https://getcomposer.org/installer');" | sudo php -- --install-dir=/usr/local/bin/ --filename=composer
     sudo chown ${USER} ~/.composer
-    printf "\n\n"
-fi
-
-
-#============================================================================
-# Homebrew
-#============================================================================
-if [ -e /usr/local/bin/brew ]; then
-    printf "\n\033[1;37mHomebrew has already been installed.\033[0m\n\n"
-else
-    printf "\n\033[1;37mInstalling Homebrew.\033[0m\n"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     printf "\n\n"
 fi
