ff9efeff9a34f2a1259452ae56d9f92cee8812fd
Douglas Smith <douglas.smith@akqa.com>
2020-04-03 15:26:04 -0400

    Updating check value

--- software/install.sh
+++ software/install.sh
@@ -25,7 +25,7 @@ fi
 # AWS CLI
 # Source: https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-mac.html#cliv2-mac-install-cmd
 name="AWS Command Line Interface"
-check="/usr/local/aws-cli"
+check="/usr/local/bin/aws"
 if [ ! -e "$check" ]; then
     curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
     sudo installer -pkg ./AWSCLIV2.pkg -target /
