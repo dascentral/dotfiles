1d2756b976d3b086c4448d78447189062bd83345
Douglas Smith <douglas.smith@akqa.com>
2020-04-03 15:25:15 -0400

    Adding the AWS CLI

--- software/install.sh
+++ software/install.sh
@@ -19,6 +19,21 @@ fi
 
 
 #============================================================================
+# Package Installations
+#============================================================================
+
+# AWS CLI
+# Source: https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-mac.html#cliv2-mac-install-cmd
+name="AWS Command Line Interface"
+check="/usr/local/aws-cli"
+if [ ! -e "$check" ]; then
+    curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+    sudo installer -pkg ./AWSCLIV2.pkg -target /
+fi
+
+
+
+#============================================================================
 # Composer installations
 #============================================================================
 
