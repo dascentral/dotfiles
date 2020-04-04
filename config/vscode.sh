384db162ed25cecf88786c397f01083cfc3a47c0
Douglas Smith <douglas.smith@akqa.com>
2020-04-04 07:50:44 -0400

    Removing the sublime settings extension

--- config/vscode.sh
+++ config/vscode.sh
@@ -59,9 +59,6 @@ code --install-extension $extension
 extension="ms-azuretools.vscode-docker"
 code --install-extension $extension
 
-extension-"ms-vscode.sublime-keybindings"
-code --install-extension $extension
-
 extension="neilbrayfield.php-docblocker"
 code --install-extension $extension
 
