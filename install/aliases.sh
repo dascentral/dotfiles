e16beac9404a6077c24826a20a6cd2d04f6c6361
Douglas Smith <douglas.smith@akqa.com>
2020-07-04 07:56:00 -0400

    Renaming files

--- install/aliases.sh
+++ install/aliases.sh
@@ -8,11 +8,16 @@
 printf "\033[1;37mCopying aliases into place.\033[0m"
 
 # Copy into place
-cp /Users/$USER/dotfiles/aliases/aliases /Users/$USER/.aliases
+cp /Users/$USER/dotfiles/shell/.aliases /Users/$USER/.aliases
 
-# Include any custom aliases stored locally
+# Include any local aliases
 if [ -e "/Users/$USER/.aliases-local" ]; then
     cat /Users/$USER/.aliases-local >> /Users/$USER/.aliases
+fi
+
+# Include any private aliases
+if [ -e "/Users/$USER/.aliases-private" ]; then
+    cat /Users/$USER/.aliases-private >> /Users/$USER/.aliases
 fi
 
 # Wrap it up
