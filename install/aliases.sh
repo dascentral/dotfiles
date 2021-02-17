b5136fbcf4ac57675eb3176658d56682be20ab85
Douglas Smith <douglas.smith@akqa.com>
2021-02-17 07:26:18 -0500

    Syntaxing

--- install/aliases.sh
+++ install/aliases.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 #===================================================================================
 # Alias Installlation
@@ -8,16 +8,16 @@
 printf "\033[1;37mCopying aliases into place.\033[0m"
 
 # Copy into place
-cp /Users/$USER/dotfiles/shell/.aliases /Users/$USER/.aliases
+cp /Users/${USER}/dotfiles/shell/.aliases /Users/${USER}/.aliases
 
 # Include any local aliases
-if [ -e "/Users/$USER/.aliases-local" ]; then
-    cat /Users/$USER/.aliases-local >> /Users/$USER/.aliases
+if [ -e "/Users/${USER}/.aliases-local" ]; then
+    cat /Users/${USER}/.aliases-local >> /Users/${USER}/.aliases
 fi
 
 # Include aliases from my private dotfiles repository
-if [ -e "/Users/$USER/dotfiles-private/shell/.aliases" ]; then
-    cat /Users/$USER/dotfiles-private/shell/.aliases >> /Users/$USER/.aliases
+if [ -e "/Users/${USER}/dotfiles-private/shell/.aliases" ]; then
+    cat /Users/${USER}/dotfiles-private/shell/.aliases >> /Users/${USER}/.aliases
 fi
 
 # Wrap it up
