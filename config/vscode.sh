3a181fcaed7a61c209b1d1b11c83716e30d27b61
Douglas Smith <douglas.smith@akqa.com>
2021-02-25 07:12:58 -0500

    Beginning refactor

--- config/vscode.sh
+++ config/vscode.sh
@@ -1,20 +1,30 @@
 #!/usr/bin/env bash
 
-# at this time, this script is NOT idempotent
-if [ -e /Users/${USER}/Library/Application\ Support/Code/User ]; then
-    printf "\n\033[1;37mVisual Studio code has already been configured.\033[0m\n\n"
-    exit 0;
+source /Users/${USER}/dotfiles/bin/.functions
+
+# configure location of User folder
+#basepath="/Users/${USER}/Library/Application\ Support/Code/User2"
+
+# create the User folder
+mkdir -pv "/Users/${USER}/Library/Application Support/Code/User"
+
+# replace local settings with those in this repository
+if [ ! -L "/Users/${USER}/Library/Application Support/Code/User/settings.json" ]; then
+    rm -rf /Users/${USER}/Library/Application\ Support/Code/User/settings.json
+    ln -s /Users/${USER}/dotfiles/settings/VSCode/User/settings.json /Users/${USER}/Library/Application\ Support/Code/User/settings.json
+    info "VS Code settings linked to repository"
 fi
 
+# replace local key bindings with those in this repository
+if [ ! -L "/Users/${USER}/Library/Application Support/Code/User/keybindings.json" ]; then
+    rm -rf /Users/${USER}/Library/Application\ Support/Code/User/keybindings.json
+    ln -s /Users/${USER}/dotfiles/settings/VSCode/User/keybindings.json /Users/${USER}/Library/Application\ Support/Code/User/keybindings.json
+    info "VS Code key bindings linked to repository"
+fi
 
-# Ensure we have a user folder
-mkdir /Users/${USER}/Library/Application\ Support/Code/User
-
-
-# Configure for use of cloud-stored settings
-rm /Users/${USER}/Library/Application\ Support/Code/User/settings.json
-rm /Users/${USER}/Library/Application\ Support/Code/User/keybindings.json
-rm -rf /Users/${USER}/Library/Application\ Support/Code/User/snippets
-ln -s /Users/${USER}/dotfiles/settings/VSCode/User/settings.json settings.json
-ln -s /Users/${USER}/dotfiles/settings/VSCode/User/keybindings.json keybindings.json
-ln -s /Users/${USER}/dotfiles/settings/VSCode/User/snippets snippets
+# replace local snippets with those in this repository
+# if [ -L "/Users/${USER}/Library/Application\ Support/Code/User/snippets" ]; then
+#     info "snippets folder exists"
+# fi
+# rm -rf /Users/${USER}/Library/Application\ Support/Code/User/snippets
+# ln -s /Users/${USER}/dotfiles/settings/VSCode/User/snippets snippets
