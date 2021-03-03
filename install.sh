47b836697804808d8b4296857c4fee2c002af311
Douglas Smith <douglas.smith@akqa.com>
2021-03-03 10:13:14 -0500

    Adding iTerm2 config to the install script

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # BEGIN configurations
 
 /Users/${USER}/dotfiles/config/vscode.sh
+/Users/${USER}/dotfiles/config/iterm2.sh
 
 # END configurations
 ###########################################################################################
