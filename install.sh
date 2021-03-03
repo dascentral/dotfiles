51bd53f1890dfd09e0715e213157fd6ed656f150
Douglas Smith <douglas.smith@akqa.com>
2021-03-03 10:12:21 -0500

    Turning on the sudo

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 source /Users/${USER}/dotfiles/shell/.functions
 
 # Ash for password before we begin
-#sudo -v
+sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until script has finished
-#while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###########################################################################################
 # BEGIN installation
