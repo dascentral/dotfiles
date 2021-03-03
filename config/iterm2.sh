d8c4df6cff6eba16c2e89f372b5efa5bba8b9da1
Douglas Smith <douglas.smith@akqa.com>
2021-03-03 10:11:57 -0500

    Disabling the iTerm script

--- config/iterm2.sh
+++ config/iterm2.sh
@@ -2,24 +2,26 @@
 
 source /Users/${USER}/dotfiles/shell/.functions
 
-# confirm app has been installed
-if [ ! -e "/Applications/iTerm.app" ]; then
-    abort "iTerm2 has not been installed."
-fi
+warn "iTerm configuration script is currently disabled"
 
-# confirm the expected settings location exists
-if [ ! -e "/Users/${USER}/GDrive/Documents/Software/iTerm2" ]; then
-    abort "iTerm2 settings folder does not exist."
-fi
+# # confirm app has been installed
+# if [ ! -e "/Applications/iTerm.app" ]; then
+#     abort "iTerm2 has not been installed."
+# fi
 
-# Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/GDrive/Documents/Software/iTerm2"
+# # confirm the expected settings location exists
+# if [ ! -e "/Users/${USER}/GDrive/Documents/Software/iTerm2" ]; then
+#     abort "iTerm2 settings folder does not exist."
+# fi
 
-# Tell iTerm2 to use the custom preferences in the directory
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+# # Specify the preferences directory
+# defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/GDrive/Documents/Software/iTerm2"
 
-# Don’t display the prompt when quitting iTerm
-defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+# # Tell iTerm2 to use the custom preferences in the directory
+# defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
-# Install and configure Oh My Zsh
-${DOTFILES}/config/oh-my-zsh.sh
+# # Don’t display the prompt when quitting iTerm
+# defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+
+# # Install and configure Oh My Zsh
+# ${DOTFILES}/config/oh-my-zsh.sh
