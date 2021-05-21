f30db3c38b8feaa20b5b97447e73bf0971df1e50
Douglas Smith <douglas.smith@akqa.com>
2021-05-21 14:55:33 -0400

    Using PRIVATE variable

--- config/sublime.sh
+++ config/sublime.sh
@@ -4,7 +4,7 @@ source /Users/${USER}/dotfiles/shell/.functions
 
 # configure User folder
 LOCALPATH="${HOME}/Library/Application Support/Sublime Text 3/Packages/User"
-CLOUDPATH="${HOME}/.private/settings/Sublime/User"
+CLOUDPATH="${PRIVATE}/settings/Sublime/User"
 
 # confirm the expected settings location exists
 if [ ! -e "${CLOUDPATH}" ]; then
