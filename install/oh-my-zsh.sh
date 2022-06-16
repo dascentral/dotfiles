4fd1c4a02fdb42c464ac76ecd7115c2f1efc239f
Douglas Smith <smithda@gmail.com>
2022-06-16 10:48:01 -0400

    Adding information output

--- install/oh-my-zsh.sh
+++ install/oh-my-zsh.sh
@@ -2,5 +2,6 @@
 
 # install Oh My Zsh
 if [ ! -e ~/.oh-my-zsh ]; then
-  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+    info "Installing Oh My Zsh."
+    /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 fi
