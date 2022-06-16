d4d862f0cf26880094982cc1b3944b8865f0f427
Douglas Smith <smithda@gmail.com>
2022-06-15 21:33:24 -0400

    Referencing helper function

--- install/homebrew.sh
+++ install/homebrew.sh
@@ -2,7 +2,7 @@
 
 source /Users/${USER}/.dotfiles/shell/.functions
 
-if [ ! -e /opt/homebrew/bin/brew ]; then
+if ! homebrew_installed; then
     info "Installing Homebrew."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
