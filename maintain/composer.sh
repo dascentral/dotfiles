97f3cfe1ff6e67db128a4fec90c422d715118e27
Douglas Smith <smithda@gmail.com>
2022-06-16 11:41:56 -0400

    Removing composer maintenance script

--- maintain/composer.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source /Users/${USER}/.dotfiles/shell/.functions
-
-confirm_composer_installed
-
-info "Composer self-update"
-/opt/homebrew/bin/composer self-update
-printf "\n"
