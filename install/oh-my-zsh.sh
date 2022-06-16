0287ace8abe11e8bb04dc27f40c91420166f8775
Douglas Smith <smithda@gmail.com>
2022-06-16 10:55:49 -0400

    Merging install and config for Oh My Zsh

--- install/oh-my-zsh.sh
+++ install/oh-my-zsh.sh
@@ -1,7 +1,50 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+source /Users/${USER}/.dotfiles/shell/.functions
 
 # install Oh My Zsh
 if [ ! -e ~/.oh-my-zsh ]; then
     info "Installing Oh My Zsh."
     /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+else
+    warn "Installing Oh My Zsh... already installed"
+fi
+
+# install plugins
+cd $HOME/.oh-my-zsh/custom/plugins
+declare -a links=(
+    "https://github.com/zsh-users/zsh-autosuggestions"
+    "https://github.com/zsh-users/zsh-syntax-highlighting"
+)
+for url in ${links[@]}; do
+    parts=(`echo ${url} | tr '/' ' '`)
+    plugin=${parts[${#parts[@]}-1]}
+    if [ ! -e ${plugin} ]; then
+        info "Installing Zsh plugin - ${plugin}"
+        git clone ${url}
+        printf "\n"
+    else
+        warn "Installing Zsh plugin - ${plugin}... already installed"
+    fi
+done
+
+# custom install due to custom naming convention
+source="https://github.com/jessarcher/zsh-artisan.git"
+plugin="artisan"
+if [ ! -e ${plugin} ]; then
+    info "Installing Zsh plugin - jessarcher/zsh-artisan"
+    git clone ${source} ${plugin}
+    printf "\n"
+else
+    warn "Installing Zsh plugin - jessarcher/zsh-artisan... already installed"
+fi
+
+# create symbolic link to .zshrc
+if [ ! -L ~/.zshrc ]; then
+    info "Linking .zshrc to dotfiles"
+    rm -rf /Users/${USER}/.zshrc
+    ln -s "${HOME}/.dotfiles/shell/.zshrc" "/Users/${USER}/.zshrc"
+    printf "\n"
+else
+    warn "Linking .zshrc to dotfiles... already linked"
 fi
