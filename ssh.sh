101c3c9ec43357b31b8c2ab5c2df8b5c06023992
Douglas Smith <smithda@gmail.com>
2022-06-16 09:02:16 -0400

    Adding ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+source /Users/${USER}/dotfiles/shell/.functions
+
+# Ash for password before we begin
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+###########################################################################################
+# BEGIN installation
+
+${DOTFILES}/install/oh-my-zsh.sh
+${DOTFILES}/install/homebrew.sh
+${DOTFILES}/install/composer.sh
+
+# END installations
+###########################################################################################
+# BEGIN configurations
+
+${DOTFILES}/config/iterm2.sh
+${DOTFILES}/config/oh-my-zsh.sh
+${DOTFILES}/config/php.sh
+${DOTFILES}/config/sublime.sh
+${DOTFILES}/config/vscode.sh
+
+# END configurations
+###########################################################################################
