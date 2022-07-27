aaf173b9d2fbeea900af920c8eec205da7ce945c
Douglas Smith <smithda@gmail.com>
2022-07-27 12:39:44 -0400

    Removing comments

--- maintain/homebrew.sh
+++ maintain/homebrew.sh
@@ -4,22 +4,18 @@ source /Users/${USER}/.dotfiles/shell/.functions
 
 confirm_homebrew_installed
 
-# Homebrew Upgrade
 info "Brew upgrade."
 brew upgrade
 printf "\n"
 
-# Homebrew Update
 info "Brew update."
 brew update
 printf "\n"
 
-# Homebrew cleanup
 info "Brew cleanup."
 brew cleanup
 printf "\n"
 
-# Homebrew doctor
 info "Brew doctor."
 brew doctor
 printf "\n"
