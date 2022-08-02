356ae766e2885a36628043cc5b9bb427d3474c6c
Douglas Smith <smithda@gmail.com>
2022-08-02 11:43:52 -0500

    Commenting

--- maintain/macos.sh
+++ maintain/macos.sh
@@ -2,5 +2,9 @@
 
 source /Users/${USER}/.dotfiles/shell/.functions
 
+# Recent attempts to automatically configure system settings through the use
+# of the ".macos" file results in some system instability. I would not
+# recommend the use of this command without an exhaustive evaluation.
+
 # macOS settings
-# source .macos?
+# source .macos
