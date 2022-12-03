13a8b1cf5386f9e1abc8d533db4e21c5e105919f
Douglas Smith <smithda@gmail.com>
2022-12-03 13:30:16 -0500

    Disabling manual sync

--- config/sublime.sh
+++ config/sublime.sh
@@ -2,24 +2,26 @@
 
 source /Users/${USER}/.dotfiles/shell/.functions
 
+# Settings sync is now managed by Mackup
+
 # TODO: Remove dependency on my private dotfiles repository.
 
 # configure User folder
-LOCALPATH="${HOME}/Library/Application Support/Sublime Text/Packages/User"
-CLOUDPATH="${PRIVATE}/settings/Sublime/User"
+# LOCALPATH="${HOME}/Library/Application Support/Sublime Text/Packages/User"
+# CLOUDPATH="${PRIVATE}/settings/Sublime/User"
 
 # confirm the expected settings location exists
-if [ ! -e "${CLOUDPATH}" ]; then
-    abort "Sublime settings folder does not exist.\n"
-fi
+# if [ ! -e "${CLOUDPATH}" ]; then
+#     abort "Sublime settings folder does not exist.\n"
+# fi
 
 # link to cloud storage
-if [ ! -L "${LOCALPATH}" ]; then
-    info "Configuring Sublime Text"
-    rm -rf "${LOCALPATH}"
-    ln -s "${CLOUDPATH}" "${LOCALPATH}"
-    printf "\n"
-fi
+# if [ ! -L "${LOCALPATH}" ]; then
+#     info "Configuring Sublime Text"
+#     rm -rf "${LOCALPATH}"
+#     ln -s "${CLOUDPATH}" "${LOCALPATH}"
+#     printf "\n"
+# fi
 
 # create a symbolic link to the Sublime executable that can be executed globally
 if [ ! -e /usr/local/bin/subl ]; then
