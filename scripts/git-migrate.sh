f3320a1066d389b54043ef505666dc7c77df99cc
Douglas Smith <smithda@gmail.com>
2024-01-02 14:13:26 -0500

    Adding a git migration script

---
+++ scripts/git-migrate.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+source /Users/${USER}/.dotfiles/shell/.functions
+
+DIR="repository_migration"
+SOURCE=""
+TARGET=""
+
+info "Creating working directory."
+mkdir ${DIR}
+
+printf "\n"
+info "Cloning source repository."
+git clone --bare ${SOURCE} ${DIR}
+
+printf "\n"
+info "Pushing a mirror to the target repository."
+git -C ${DIR} push --mirror ${TARGET}
+
+printf "\n"
+info "Removing working directory."
+rm -rf ${DIR}
