98a3556998a7299bec320ce30abc54c4e902f20c
Douglas Smith <smithda@gmail.com>
2024-08-07 12:02:21 -0400

    Referencing new scripts folder

--- install.sh
+++ install.sh
@@ -11,9 +11,9 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 ###############################################################################
 # BEGIN installation
 
-$DOTFILES/install/oh-my-zsh.sh
-$DOTFILES/install/homebrew.sh
-$DOTFILES/install/composer.sh
+$DOTFILES/scripts/oh-my-zsh.sh
+$DOTFILES/scripts/homebrew.sh
+$DOTFILES/scripts/composer.sh
 
 # END installations
 ###############################################################################
