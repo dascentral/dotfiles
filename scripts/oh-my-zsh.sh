bc5b252eb523247acf7f61a0cd3ff2dc2e81c010
Douglas Smith <smithda@gmail.com>
2024-08-07 12:01:31 -0400

    Making oh-my-zsh script idempotent

--- scripts/oh-my-zsh.sh
+++ scripts/oh-my-zsh.sh
@@ -3,14 +3,18 @@
 source /Users/${USER}/.dotfiles/shell/.functions
 
 # install Oh My Zsh
+info "Installing Oh My Zsh."
 if [ ! -e ~/.oh-my-zsh ]; then
-    info "Installing Oh My Zsh."
     /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+    success "Installation complete"
 else
-    warn "Installing Oh My Zsh... already installed"
+    line "Already installed."
 fi
+printf "\n"
+
 
 # install plugins
+info "Installing Zsh plugins."
 cd $HOME/.oh-my-zsh/custom/plugins
 declare -a links=(
     "https://github.com/zsh-users/zsh-autosuggestions"
@@ -22,11 +26,11 @@ for url in ${links[@]}; do
     plugin=${parts[${#parts[@]}-1]}
 
     if [ ! -e ${plugin} ]; then
-        info "Installing Zsh plugin - ${plugin}"
         git clone ${url}
-        printf "\n"
+        success "${plugin} - installed"
+        # printf "\n"
     else
-        warn "Installing Zsh plugin - ${plugin}... already installed"
+        line "${plugin} - already installed"
     fi
 done
 
@@ -34,21 +38,24 @@ done
 source="https://github.com/jessarcher/zsh-artisan.git"
 plugin="artisan"
 if [ ! -e ${plugin} ]; then
-    info "Installing Zsh plugin - jessarcher/zsh-artisan"
     git clone ${source} ${plugin}
-    printf "\n"
+    success "jessarcher/zsh-artisan - installed"
+    # printf "\n"
 else
-    warn "Installing Zsh plugin - jessarcher/zsh-artisan... already installed"
+    line "jessarcher/zsh-artisan - already installed"
 fi
+printf "\n"
+
 
 # create symbolic link to .zshrc
+info "Creating symbolic link for Zsh config."
 if [ ! -L ~/.zshrc ]; then
-    info "Linking .zshrc to dotfiles"
     rm -rf /Users/${USER}/.zshrc
     ln -s "${HOME}/.dotfiles/shell/.zshrc" "/Users/${USER}/.zshrc"
-    printf "\n"
+    success "Symbolic link created"
+    # printf "\n"
 else
-    warn "Linking .zshrc to dotfiles... already linked"
+    line "Already linked."
 fi
 
 printf "\n"
