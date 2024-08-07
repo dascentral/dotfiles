e5c431036e542713d918c1aa25b8228860b055fe
Douglas Smith <smithda@gmail.com>
2024-08-07 11:50:31 -0400

    Merging Homebrew scripts into a single file.
    We either install or we maintain.

---
+++ scripts/homebrew.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+source /Users/${USER}/.dotfiles/shell/.functions
+
+if ! homebrew_installed; then
+    # Homebrew Installation
+    info "Installing Homebrew."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    printf "\n\n"
+
+    info "Executing Brewfile"
+    brew bundle --file ${DOTFILES}/Brewfile
+    printf "\n\n"
+else
+    # Homebrew Maintenance
+    info "Brew upgrade."
+    brew upgrade
+    printf "\n"
+
+    info "Brew update."
+    brew update
+    printf "\n"
+
+    info "Brew cleanup."
+    brew cleanup
+    printf "\n"
+
+    info "Brew doctor."
+    brew doctor
+    printf "\n"
+fi
