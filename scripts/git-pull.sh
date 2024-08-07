a462844fdd78d839797e3290394c3d224d690f99
Douglas Smith <smithda@gmail.com>
2024-08-07 12:02:03 -0400

    Renaming prepare to git-pull

--- scripts/git-pull.sh
+++ scripts/git-pull.sh
@@ -2,8 +2,8 @@
 
 source /Users/${USER}/.dotfiles/shell/.functions
 
-# While I am happy to share my dotfiles with the world, I maintain
-# a few sensitive items within a separate private repository.
+# While I am happy to share my dotfiles with the world, I keep
+# a few sensitive items within another private repository.
 if [ -e "${PRIVATE}" ]; then
     info "Pulling latest from Git: ${PRIVATE}"
     cd ${PRIVATE}
