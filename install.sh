5f2b30801eb3f7fa931559644411bd1b537a550f
Douglas Smith <smithda@gmail.com>
2025-04-18 14:19:16 -0400

    WIP

--- install.sh
+++ install.sh
@@ -10,15 +10,15 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # run scripts
 ./scripts/oh-my-zsh.sh
+# script will not complete successfully if oh-my-zsh is installed first
 exit
 
-# ./scripts/homebrew.sh
-# ./scripts/composer.sh
-# ./scripts/mackup.sh
+./scripts/homebrew.sh
+./scripts/composer.sh
+./scripts/mackup.sh
+./scripts/sublime.sh
+./scripts/vscode.sh
+./scripts/git.sh
+./scripts/ssh.sh
 
 # TODO: Determine when/how to run "mackup restore -v -f"
-
-# ./scripts/sublime.sh
-# ./scripts/vscode.sh
-# ./scripts/git.sh
-# ./scripts/ssh.sh
