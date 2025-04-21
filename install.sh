fa73d60e5b97f4e81a99a998914d78e72278dec2
Douglas Smith <smithda@gmail.com>
2025-04-21 11:00:00 -0400

    Reordering and adding Cursor

--- install.sh
+++ install.sh
@@ -8,17 +8,19 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# run scripts
-./scripts/oh-my-zsh.sh
-# script will not complete successfully if oh-my-zsh is installed first
-exit
-
 ./scripts/homebrew.sh
 ./scripts/composer.sh
-./scripts/mackup.sh
-./scripts/sublime.sh
-./scripts/vscode.sh
 ./scripts/git.sh
 ./scripts/ssh.sh
+./scripts/vscode.sh
+./scripts/cursor.sh
+# Note that while I have configured Mackup, I am not currently using it.
+./scripts/mackup.sh
+
+# Install after the initial installation completes
+#./scripts/oh-my-zsh.sh
+
+# Install after opening Sublime Text, installing Package Control, and installing "Monokai Pro"
+#./scripts/sublime.sh
 
 # TODO: Determine when/how to run "mackup restore -v -f"
