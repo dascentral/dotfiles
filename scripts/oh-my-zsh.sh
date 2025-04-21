ff884cddc26fbaca959a206ed3baca87bf110332
Douglas Smith <smithda@gmail.com>
2025-04-21 17:50:57 -0400

    Removing dashes

--- scripts/oh-my-zsh.sh
+++ scripts/oh-my-zsh.sh
@@ -64,6 +64,6 @@ printf "\n"
 
 # Display restart message if Oh My Zsh was just installed
 if [ $installed -eq 1 ]; then
-    info "Oh-My-Zsh was just installed. You will want to exit this shell and restart terminal."
+    info "Oh My Zsh was just installed. You will want to exit this shell and restart terminal."
     printf "\n"
 fi
