49451cb95c9748dee5928a626e19d081ba113164
Douglas Smith <smithda@gmail.com>
2025-04-22 08:33:34 -0400

    Using DOTFILES variable

--- scripts/git.sh
+++ scripts/git.sh
@@ -5,7 +5,7 @@ source /Users/${USER}/.dotfiles/shell/.functions
 info "Linking Git configuration to dotfiles."
 if [ ! -L ${HOME}/.gitconfig ]; then
     rm -rf ${HOME}/.gitconfig
-    ln -s "${HOME}/.dotfiles/config/Git/.gitconfig" "/Users/${USER}/.gitconfig"
+    ln -s "${HOME}/${DOTFILES}/config/Git/.gitconfig" "/Users/${USER}/.gitconfig"
     success ".gitconfig - linked"
 else
     line ".gitconfig - already linked"
