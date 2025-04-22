3e5045bac61ee3c83f2c8ae4ea531490fa1755d1
Douglas Smith <smithda@gmail.com>
2025-04-22 08:43:02 -0400

    Commenting

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# sudo chown ${USER} /usr/local/bin
+
 ./scripts/homebrew.sh
 ./scripts/composer.sh
 ./scripts/git.sh
