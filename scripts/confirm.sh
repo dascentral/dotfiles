867b03455b3599fbb7e86f8913527c255f8719ed
Douglas Smith <smithda@gmail.com>
2025-04-22 12:28:33 -0400

    Adding a confirm script prior to execution

---
+++ scripts/confirm.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+source /Users/${USER}/.dotfiles/shell/.functions
+
+clean=true
+
+if ! fc-list | grep -q "Fira Code"; then
+    warn "⚠️ Fira Code font is not installed."
+    clean=false
+else
+    success "✅ Fira Code font is installed."
+fi
+
+if [ ! -f "${DOTFILES}/Brewfile" ]; then
+    warn "⚠️ Brewfile not found in the dotfiles root."
+    clean=false
+else
+    success "✅ Brewfile exists in root directory."
+fi
+
+printf "\n"
+
+if [ "$clean" = false ]; then
+    info "Please resolve warnings to continue."
+    exit 1
+fi
+
+exit 0
