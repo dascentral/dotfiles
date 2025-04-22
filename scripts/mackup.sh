843d2f851d6b42bb51e3fe33447876c94430cd78
Douglas Smith <smithda@gmail.com>
2025-04-22 08:41:18 -0400

    Using DOTFILES variable

--- scripts/mackup.sh
+++ scripts/mackup.sh
@@ -5,7 +5,7 @@ source /Users/${USER}/.dotfiles/shell/.functions
 info "Linking Mackup configuration to dotfiles."
 if [ ! -L ~/.mackup.cfg ]; then
     rm -rf /Users/${USER}/.mackup.cfg
-    ln -s "${HOME}/.dotfiles/.mackup.cfg" "/Users/${USER}/.mackup.cfg"
+    ln -s "${HOME}/${DOTFILES}/.mackup.cfg" "/Users/${USER}/.mackup.cfg"
     success ".mackup.cfg - linked"
 else
     line ".mackup.cfg - already linked"
