38b7f22648f8e82ddd65f91889d33fc2707a1a01
Douglas Smith <smithda@gmail.com>
2025-04-22 12:36:32 -0400

    Reordering checks

--- scripts/confirm.sh
+++ scripts/confirm.sh
@@ -7,18 +7,19 @@ clean=true
 info "Checking system readiness."
 printf "\n"
 
-if ! fc-list | grep -q "Fira Code"; then
-    warn "⚠️ Fira Code font is not installed."
-    clean=false
-else
-    success "✅ Fira Code font is installed."
-fi
 
 if [ ! -f "${DOTFILES}/Brewfile" ]; then
     warn "⚠️ Brewfile not found in the dotfiles root."
     clean=false
 else
     success "✅ Brewfile exists in root directory."
+fi
+
+if ! fc-list | grep -q "Fira Code"; then
+    warn "⚠️ Fira Code font is not installed."
+    clean=false
+else
+    success "✅ Fira Code font is installed."
 fi
 
 printf "\n"
