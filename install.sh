2d7d7cd6d44f57a15caa72248a0ac53c8f2c506f
Douglas Smith <smithda@gmail.com>
2025-04-22 08:43:11 -0400

    Reordering scripts. Oh My Zsh is now first.

--- install.sh
+++ install.sh
@@ -8,18 +8,19 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# sudo chown ${USER} /usr/local/bin
-
-./scripts/homebrew.sh
-./scripts/composer.sh
-./scripts/git.sh
-./scripts/ssh.sh
-./scripts/vscode.sh
-./scripts/cursor.sh
-./scripts/mackup.sh
+# first run scripts
 ./scripts/oh-my-zsh.sh
 
-# Install after opening Sublime Text, installing Package Control, and installing "Monokai Pro"
-#./scripts/sublime.sh
+# sudo chown ${USER} /usr/local/bin
+
+# second run scripts
+./scripts/homebrew.sh
+./scripts/git.sh
+./scripts/ssh.sh
+./scripts/mackup.sh
+./scripts/vscode.sh
+./scripts/cursor.sh
+./scripts/composer.sh
+./scripts/sublime.sh
 
 # TODO: The Mackup script only configures Mackup.Determine when/how to run "mackup restore -v -f"
