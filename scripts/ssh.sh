dce02ab34f1f7adce4613f8cc64cea221b770921
Douglas Smith <smithda@gmail.com>
2025-04-22 08:33:38 -0400

    Using PRIVATE variable

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -6,7 +6,7 @@ if [ -e "${PRIVATE}" ]; then
     info "Linking SSH configuration to the private dotfiles."
     if [ ! -L ${HOME}/.ssh/config ]; then
         rm -rf ${HOME}/.ssh/config
-        ln -s "${HOME}/.private/config/ssh/config" "${HOME}/.ssh/config"
+        ln -s "${HOME}/${PRIVATE}/config/ssh/config" "${HOME}/.ssh/config"
         success "~/.ssh/config - linked"
     else
         line "~/.ssh/config - already linked"
