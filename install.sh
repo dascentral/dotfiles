f9dd5d584dbd649f21067d5d40ba357f1b173454
Douglas Smith <smithda@gmail.com>
2025-04-29 10:25:08 -0400

    Reordering scripts

--- install.sh
+++ install.sh
@@ -14,12 +14,12 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # TODO: sudo chown ${USER} /usr/local/bin
 
 ./scripts/homebrew.sh
+./scripts/composer.sh
 ./scripts/git.sh
 ./scripts/ssh.sh
 ./scripts/mackup.sh
-./scripts/vscode.sh
 ./scripts/cursor.sh
-./scripts/composer.sh
+./scripts/vscode.sh
 ./scripts/sublime.sh
 
 # TODO: Automatically run ./scripts/macos.sh?
