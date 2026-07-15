f021737273360d4743f455fe8cc0d1747b236171
Douglas Smith <smithda@gmail.com>
2026-07-15 09:07:13 -0400

    feat: apply global .gitignore

--- scripts/git.sh
+++ scripts/git.sh
@@ -11,4 +11,12 @@ else
     line ".gitconfig - already linked"
 fi
 
+if [ ! -L ${HOME}/.gitignore_global ]; then
+    rm -f ${HOME}/.gitignore_global
+    ln -s "${DOTFILES}/config/Git/.gitignore_global" "${HOME}/.gitignore_global"
+    success ".gitignore_global - linked"
+else
+    line ".gitignore_global - already linked"
+fi
+
 printf "\n"
